fab188a1a344a0be96c87449b53164099f501585
Mochammad Machfudh <cmmachfudh@gmail.com>
2022-07-06 16:49:24 +0700

    Added function scolltoView

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let btnSlider = document.querySelectorAll(".dot-slide")
 let itemSlider = document.querySelectorAll(".tbc-items")
+let navItem = document.querySelectorAll(".nav-item")
+let backToTop = document.querySelector(".back-to-top a")
 
 btnSlider.forEach(btn => {
   btn.addEventListener('click', function (event) {
@@ -15,4 +17,24 @@ btnSlider.forEach(btn => {
       document.querySelector(`#${target}`).classList.add("active")
     }
   })
+})
+
+function scrollToView(el){
+  el.scrollIntoView({
+    behavior: 'smooth'
+  })
+}
+
+navItem.forEach(item => {
+  item.addEventListener('click', function (event) {
+    event.preventDefault();
+    let target = this.querySelector("a").getAttribute("href")
+    scrollToView(document.querySelector(target))
+  })
+})
+
+backToTop.addEventListener('click', function (event) {
+  event.preventDefault();
+  let target = this.getAttribute("href")
+  scrollToView(document.querySelector(target))
 })
